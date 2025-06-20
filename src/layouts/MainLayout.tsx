8de5312fcee660ed1e28f8a3674a7d8e9ffc387e
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-06-20 17:49:46 -0300

    agregar estilos al header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,42 +4,59 @@ import './MainLayout.css'
 
 export function MainLayout() {
   return (
-    <div className='site-layout'>
-      <header className='site-header'>
-        <h1>
-          <Link to="/">Demo Accesibilidad</Link>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li>
-              <Link to="/live-demo">Live Regions</Link>
-            </li>
-            <li>
-              <Link to="/about">Acerca de</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <>
+      <SkipLink />
+      <div className="site-layout">
+        <header className="site-header">
+          <div className="container">
+            <h1 className="h3">
+              <Link to="/">Demo Accesibilidad</Link>
+            </h1>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Inicio</Link>
+                </li>
+                <li>
+                  <Link to="/live-demo">Live Regions</Link>
+                </li>
+                <li>
+                  <Link to="/about">Acerca de</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
 
-      <main id="main-content" className='site-main'>
-        <Outlet />
-      </main>
+        <main id="main-content" className="site-main container">
+          <Outlet />
+        </main>
 
-      <footer className='site-footer'>
-        <p>
-          Creado con ♥️ por{' '}
-          <a
-            href="https://github.com/msilenzi"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Manu
-          </a>
-        </p>
-      </footer>
-    </div>
+        <footer className="site-footer">
+          <p>
+            Creado con ♥️ por{' '}
+            <a
+              href="https://github.com/msilenzi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Manu
+            </a>
+          </p>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+/**
+ * Componente que muestra un enlace para saltar al contenido principal.
+ * Esencial para usuarios de teclado.
+ */
+function SkipLink() {
+  return (
+    <a href="#main-content" className="skip-link">
+      Ir al contenido principal
+    </a>
   )
 }
