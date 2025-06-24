b47f992ee5bcd1b800b8951f5998fa025aa3810b
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-06-23 22:17:14 -0300

    agregar contenido a <LiveRegionPage />

--- src/pages/LiveRegionPage.tsx
+++ src/pages/LiveRegionPage.tsx
@@ -1,3 +1,53 @@
+import type { Quote } from '../data/quotes'
+
+import { useEffect } from 'react'
+
+import { useQuotes } from '../hooks/useQuotes'
+
+import './LiveRegionPage.css'
+
 export function LiveRegionPage() {
-  return <>LiveRegionPage</>
+  const { isLoading, quote, changeQuote } = useQuotes()
+
+  useEffect(() => void changeQuote(), [changeQuote])
+
+  const loading = isLoading || !quote
+
+  return (
+    <section>
+      <h1>Ejemplo de regiones dinámicas</h1>
+      <p className="live-region__description">
+        Esta sección es una pequeña demostración que simula la carga asíncrona
+        de elementos en el DOM.
+      </p>
+
+      <div className="live-region__quote">
+        {loading ? <Loading /> : <QuoteComponent {...quote} />}
+      </div>
+
+      <button
+        className="btn btn--primary live-region__btn"
+        type="button"
+        onClick={() => void changeQuote()}
+        disabled={loading}
+      >
+        Cambiar frase
+      </button>
+    </section>
+  )
+}
+
+function QuoteComponent({ quote, author }: Quote) {
+  return (
+    <figure className="quote">
+      <blockquote>
+        <p>{quote}</p>
+      </blockquote>
+      <figcaption>— {author}</figcaption>
+    </figure>
+  )
+}
+
+function Loading() {
+  return <p>Cargando...</p>
 }
